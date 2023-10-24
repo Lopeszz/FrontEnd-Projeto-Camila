5e4b45fa41b2052d578b0f28d9e0f5b8528dec43
Giovani <giovanilopesmcp@gmail.com>
2023-10-24 15:31:22 -0300

    Separando Header

---
+++ header.php
@@ -0,0 +1,167 @@
+<!-- Start Header Area -->
+<header class="header-section d-none d-xl-block">
+    <div class="header-wrapper">
+        <div class="header-bottom header-bottom-color--golden section-fluid sticky-header sticky-color--golden">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 d-flex align-items-center justify-content-between">
+                        <!-- Start Header Logo -->
+                        <div class="header-logo">
+                            <div class="logo">
+                                <a href="index.html"><img src="assets/images/logo/logo_black.png" alt=""></a>
+                            </div>
+                        </div>
+                        <!-- End Header Logo -->
+
+                        <!-- Start Header Main Menu -->
+                        <div class="main-menu menu-color--black menu-hover-color--golden">
+                            <nav>
+                                <ul>
+                                    <li class="has-dropdown">
+                                        <a class="active main-menu-link" href="index.html">Home <i
+                                                class="fa fa-angle-down"></i></a>
+                                        <!-- Sub Menu -->
+                                        <ul class="sub-menu">
+                                            <li><a href="index.html">Home 1</a></li>
+                                            <li><a href="index-2.html">Home 2</a></li>
+                                            <li><a href="index-3.html">Home 3</a></li>
+                                            <li><a href="index-4.html">Home 4</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="has-dropdown has-megaitem">
+                                        <a href="product-details-default.html">Shop <i class="fa fa-angle-down"></i></a>
+                                        <!-- Mega Menu -->
+                                        <div class="mega-menu">
+                                            <ul class="mega-menu-inner">
+                                                <!-- Mega Menu Sub Link -->
+                                                <li class="mega-menu-item">
+                                                    <a href="#" class="mega-menu-item-title">Shop Layouts</a>
+                                                    <ul class="mega-menu-sub">
+                                                        <li><a href="shop-grid-sidebar-left.html">Grid Left
+                                                                Sidebar</a></li>
+                                                        <li><a href="shop-grid-sidebar-right.html">Grid Right
+                                                                Sidebar</a></li>
+                                                        <li><a href="shop-full-width.html">Full Width</a></li>
+                                                        <li><a href="shop-list-sidebar-left.html">List Left
+                                                                Sidebar</a></li>
+                                                        <li><a href="shop-list-sidebar-right.html">List Right
+                                                                Sidebar</a></li>
+                                                    </ul>
+                                                </li>
+                                                <!-- Mega Menu Sub Link -->
+                                                <li class="mega-menu-item">
+                                                    <a href="#" class="mega-menu-item-title">Other Pages</a>
+                                                    <ul class="mega-menu-sub">
+                                                        <li><a href="cart.html">Cart</a></li>
+                                                        <li><a href="empty-cart.html">Empty Cart</a></li>
+                                                        <li><a href="wishlist.html">Wishlist</a></li>
+                                                        <li><a href="compare.html">Compare</a></li>
+                                                        <li><a href="checkout.html">Checkout</a></li>
+                                                        <li><a href="login.html">Login</a></li>
+                                                        <li><a href="my-account.html">My Account</a></li>
+                                                    </ul>
+                                                </li>
+                                                <!-- Mega Menu Sub Link -->
+                                                <li class="mega-menu-item">
+                                                    <a href="#" class="mega-menu-item-title">Product Types</a>
+                                                    <ul class="mega-menu-sub">
+                                                        <li><a href="product-details-default.html">Product
+                                                                Default</a></li>
+                                                        <li><a href="product-details-variable.html">Product
+                                                                Variable</a></li>
+                                                        <li><a href="product-details-affiliate.html">Product
+                                                                Referral</a></li>
+                                                        <li><a href="product-details-group.html">Product Group</a>
+                                                        </li>
+                                                        <li><a href="product-details-single-slide.html">Product
+                                                                Slider</a></li>
+                                                    </ul>
+                                                </li>
+                                                <!-- Mega Menu Sub Link -->
+                                                <li class="mega-menu-item">
+                                                    <a href="#" class="mega-menu-item-title">Product Types</a>
+                                                    <ul class="mega-menu-sub">
+                                                        <li><a href="product-details-tab-left.html">Product Tab
+                                                                Left</a></li>
+                                                        <li><a href="product-details-tab-right.html">Product Tab
+                                                                Right</a></li>
+                                                        <li><a href="product-details-gallery-left.html">Product
+                                                                Gallery Left</a></li>
+                                                        <li><a href="product-details-gallery-right.html">Product
+                                                                Gallery Right</a></li>
+                                                        <li><a href="product-details-sticky-left.html">Product
+                                                                Sticky Left</a></li>
+                                                        <li><a href="product-details-sticky-right.html">Product
+                                                                Sticky right</a></li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                            <div class="menu-banner">
+                                                <a href="#" class="menu-banner-link">
+                                                    <img class="menu-banner-img"
+                                                        src="assets/images/banner/menu-banner.jpg" alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="has-dropdown">
+                                        <a href="blog-single-sidebar-left.html">Blog <i
+                                                class="fa fa-angle-down"></i></a>
+                                        <!-- Sub Menu -->
+                                        <ul class="sub-menu">
+                                            <li><a href="blog-grid-sidebar-left.html">Blog Grid Sidebar left</a>
+                                            </li>
+                                            <li><a href="blog-grid-sidebar-right.html">Blog Grid Sidebar Right</a>
+                                            </li>
+                                            <li><a href="blog-full-width.html">Blog Full Width</a></li>
+                                            <li><a href="blog-list-sidebar-left.html">Blog List Sidebar Left</a>
+                                            </li>
+                                            <li><a href="blog-list-sidebar-right.html">Blog List Sidebar Right</a>
+                                            </li>
+                                            <li><a href="blog-single-sidebar-left.html">Blog Single Sidebar left</a>
+                                            </li>
+                                            <li><a href="blog-single-sidebar-right.html">Blog Single Sidebar
+                                                    Right</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="has-dropdown">
+                                        <a href="#">Pages <i class="fa fa-angle-down"></i></a>
+                                        <!-- Sub Menu -->
+                                        <ul class="sub-menu">
+                                            <li><a href="faq.html">Frequently Questions</a></li>
+                                            <li><a href="privacy-policy.html">Privacy Policy</a></li>
+                                            <li><a href="404.html">404 Page</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="about-us.html">About Us</a>
+                                    </li>
+                                    <li>
+                                        <a href="contact-us.html">Contact Us</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <!-- End Header Main Menu Start -->
+
+                        <!-- Start Header Action Link -->
+                        <ul class="header-action-link action-color--black action-hover-color--golden">
+                            <li>
+                                <a href="#search">
+                                    <i class="icon-magnifier"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#offcanvas-about" class="offacnvas offside-about offcanvas-toggle">
+                                    <i class="icon-menu"></i>
+                                </a>
+                            </li>
+                        </ul>
+                        <!-- End Header Action Link -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- Start Header Area -->
