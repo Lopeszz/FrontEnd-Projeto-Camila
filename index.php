89a729d88c2add993caf2be764070e0a96215925
Alejandro <109864720+AlejandroMartins@users.noreply.github.com>
2023-10-24 17:31:07 -0300

    modify img index

--- index.php
+++ index.php
@@ -73,35 +73,11 @@
                     </div>
                 </div> <!-- End Hero Single Slider Item -->
                 <!-- Start Hero Single Slider Item -->
-                <div class="hero-single-slider-item swiper-slide">
-                    <!-- Hero Slider Image -->
-                    <div class="hero-slider-bg">
-                        <img src="assets/images/hero-slider/home-1/hero-slider-2.jpg" alt="">
-                    </div>
-                    <!-- Hero Slider Content -->
-                    <div class="hero-slider-wrapper">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-auto">
-                                    <div class="hero-slider-content">
-                                        <h4 class="subtitle">New collection</h4>
-                                        <h2 class="title">Luxy Chairs <br> Design Award </h2>
-                                        <a href="product-details-default.php" class="btn btn-lg btn-outline-golden">shop
-                                            now </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div> <!-- End Hero Single Slider Item -->
+                
             </div>
 
-            <!-- If we need pagination -->
-            <div class="swiper-pagination active-color-golden"></div>
-
-            <!-- If we need navigation buttons -->
-            <div class="swiper-button-prev d-none d-lg-block"></div>
-            <div class="swiper-button-next d-none d-lg-block"></div>
+         
+           
         </div>
     </div>
     <!-- End Hero Slider Section-->
