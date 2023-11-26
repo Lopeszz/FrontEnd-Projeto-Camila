5f05369eb3e10e86af7604d448c40569195e72bd
Vitor Dumont <ve23842@gmail.com>
2023-11-26 20:37:17 -0300

    tamanho apresentação

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ if (isset($_SESSION['user_id'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="zxx">
+<html lang="pt-br">
 
 
 <meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
