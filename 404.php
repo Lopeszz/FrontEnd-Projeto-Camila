bc985ba77b453ac09e7c226232c93bebd954ab87
Vitor Dumont <ve23842@gmail.com>
2023-11-26 19:56:30 -0300

    remove teste

--- 404.php
+++ 404.php
@@ -9,7 +9,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-87
     <title>HONO - Multi Purpose HTML Template</title>
 
     <!-- ::::::::::::::Favicon icon::::::::::::::-->
