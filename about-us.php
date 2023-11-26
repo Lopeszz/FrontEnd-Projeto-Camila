367fed8710460d6b8d324130d789c555c4f64a00
Natan <natan.lemos.pereira100@gmail.com>
2023-11-26 20:34:26 -0300

    img2

--- about-us.php
+++ about-us.php
@@ -56,21 +56,14 @@
                 <div class="col-md-6">
                     <div class="about-img" data-aos="fade-up" data-aos-delay="0">
                         <div class="img-responsive">
-                            <img src="assets/images/about/img-about.jpg" alt="">
+                            <img src="uploaded_img/lampada.jpg" alt="">
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6">
                     <div class="content" data-aos="fade-up" data-aos-delay="200">
-                        <h3 class="title">ABOUT OUR HONO STORE</h3>
-                        <h5 class="semi-title">We believe that every project existing in digital world is a result of an
-                            idea and every idea has a cause.</h5>
-                        <p>For this reason, our each design serves an idea. Our strength in design is reflected by our
-                            name, our care for details. Our specialist won't be afraid to go extra miles just to
-                            approach near perfection. We don't require everything to be perfect, but we need them to be
-                            perfectly cared for. That's a reason why we are willing to give contributions at best. Not a
-                            single detail is missed out under Billey's professional eyes.The amount of dedication and
-                            effort equals to the level of passion and determination. Get better, together as one.</p>
+                        <h3 class="title">PROJETO DE EXTENSÃO</h3>
+                        <p>O projeto de extensão realizado no Instituto Federal de Minas Gerais (IFMG), sob a coordenação da professora de história Camila Similhana, representa uma iniciativa significativa no campo da pesquisa histórica e da educação patrimonial. Com a participação ativa de seus alunos, o projeto teve como objetivo principal resgatar e preservar documentos históricos relacionados à cidade de São João Evangelista, em Minas Gerais.</p>
                     </div>
                 </div>
             </div>
