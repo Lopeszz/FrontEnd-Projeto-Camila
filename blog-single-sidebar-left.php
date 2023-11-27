9516d46dc6de6cadf34c49edf0709ab2a8f86bd4
Giovani <giovanilopesmcp@gmail.com>
2023-11-26 22:02:36 -0300

    Up search all locate

--- blog-single-sidebar-left.php
+++ blog-single-sidebar-left.php
@@ -160,21 +160,25 @@ if ($select_posts->rowCount() > 0) {
 
                         <!-- Start Single Sidebar Widget -->
                         <div class="sidebar-single-widget">
-                            <h6 class="sidebar-title">Search</h6>
+                            <h6 class="sidebar-title">Buscar</h6>
                             <div class="default-search-style d-flex">
-                                <input class="default-search-style-input-box" type="search" placeholder="Search..."
-                                    required>
-                                <button class="default-search-style-input-btn" type="submit"><i
-                                        class="fa fa-search"></i></button>
+                                <form action="search.php" method="POST" class="search-form">
+
+                                    <input class="default-search-style-input-box" type="search"
+                                        placeholder="Pesquisar...   " maxlength="100" name="search_box" required>
+                                    <button name="search_btn" class="default-search-style-input-btn" type="submit"><i
+                                            class="fa fa-search"></i></button>
+                                </form>
+
                             </div>
                         </div> <!-- End Single Sidebar Widget -->
 
                         <!-- Start Single Sidebar Widget -->
                         <div class="sidebar-single-widget">
-                            <h6 class="sidebar-title">CATEGORIES</h6>
+                            <h6 class="sidebar-title">Categorias</h6>
                             <div class="sidebar-content">
                                 <ul class="sidebar-menu">
-                                    <li><a href="#">Audio</a></li>
+                                    <li><a href="category.php?category=viagens">Viagens</a></li>
                                     <li><a href="#">Company</a></li>
                                     <li><a href="#">Gallery</a></li>
                                     <li><a href="#">Other</a></li>
@@ -182,72 +186,6 @@ if ($select_posts->rowCount() > 0) {
                                     <li><a href="#"> Uncategorized</a></li>
                                     <li><a href="#"> Video</a></li>
                                     <li><a href="#">Wordpress</a></li>
-                                </ul>
-                            </div>
-                        </div> <!-- End Single Sidebar Widget -->
-
-                        <!-- Start Single Sidebar Widget -->
-                        <div class="sidebar-single-widget">
-                            <h6 class="sidebar-title">Tags</h6>
-                            <div class="sidebar-content">
-                                <div class="tag-link">
-                                    <a href="#">Technology</a>
-                                    <a href="#">Information</a>
-                                    <a href="#">Wordpress</a>
-                                    <a href="#">Devs</a>
-                                    <a href="#">Program</a>
-                                </div>
-                            </div>
-                        </div> <!-- End Single Sidebar Widget -->
-
-                        <!-- Start Single Sidebar Widget -->
-                        <div class="sidebar-single-widget">
-                            <h6 class="sidebar-title">Meta</h6>
-                            <div class="sidebar-content">
-                                <ul class="sidebar-menu">
-                                    <li><a href="#">Log in</a></li>
-                                    <li><a href="#">Entries feed</a></li>
-                                    <li><a href="#">Comments feed</a></li>
-                                    <li><a href="#">WordPress.org</a></li>
-                                </ul>
-                            </div>
-                        </div> <!-- End Single Sidebar Widget -->
-
-                        <!-- Start Single Sidebar Widget -->
-                        <div class="sidebar-single-widget">
-                            <h6 class="sidebar-title">PRODUCT CATEGORIES</h6>
-                            <div class="sidebar-content">
-                                <ul class="sidebar-menu">
-                                    <li>
-                                        <ul class="sidebar-menu-collapse">
-                                            <!-- Start Single Menu Collapse List -->
-                                            <li class="sidebar-menu-collapse-list">
-                                                <div class="accordion">
-                                                    <a href="shop-grid-sidebar-left.html"
-                                                        class="accordion-title collapsed" data-bs-toggle="collapse"
-                                                        data-bs-target="#men-fashion" aria-expanded="false">Men <i
-                                                            class="ion-ios-arrow-right"></i></a>
-                                                    <div id="men-fashion" class="collapse">
-                                                        <ul class="accordion-category-list">
-                                                            <li><a href="#">Dresses</a></li>
-                                                            <li><a href="#">Jackets &amp; Coats</a></li>
-                                                            <li><a href="#">Sweaters</a></li>
-                                                            <li><a href="#">Jeans</a></li>
-                                                            <li><a href="#">Blouses &amp; Shirts</a></li>
-                                                        </ul>
-                                                    </div>
-                                                </div>
-                                            </li> <!-- End Single Menu Collapse List -->
-                                        </ul>
-                                    </li>
-                                    <li><a href="#">Football</a></li>
-                                    <li><a href="#"> Men's</a></li>
-                                    <li><a href="#"> Portable Audio</a></li>
-                                    <li><a href="#"> Smart Watches</a></li>
-                                    <li><a href="#">Tennis</a></li>
-                                    <li><a href="#"> Uncategorized</a></li>
-                                    <li><a href="#"> Video Games</a></li>
-                                    <li><a href="#">Women's</a></li>
                                 </ul>
                             </div>
                         </div> <!-- End Single Sidebar Widget -->
--- category.php
+++ category.php
@@ -72,7 +72,7 @@ $category = isset($_GET['category']) ? $_GET['category'] : '';
                                     <input type="hidden" name="post_id" value="<?= $post_id; ?>">
                                     <input type="hidden" name="admin_id" value="<?= $fetch_posts['admin_id']; ?>">
                                     <div class="image-box">
-                                       <a href="blog-single-sidebar-left.html" class="image-link">
+                                       <a href="blog-single-sidebar-left.php?post_id=<?= $post_id; ?>" class="image-link">
                                           <?php
                                           if ($fetch_posts['image'] != '') {
                                              ?>
@@ -94,14 +94,15 @@ $category = isset($_GET['category']) ? $_GET['category'] : '';
                                              </a>
                                           </li>
                                        </ul>
-                                       <h6 class="title"><a href="blog-single-sidebar-left.html">
+                                       <h6 class="title">
+                                          <a href="blog-single-sidebar-left.php?post_id=<?= $post_id; ?>">
                                              <?= $fetch_posts['title']; ?>
                                           </a>
                                        </h6>
                                        <p>
                                           <?= $fetch_posts['content']; ?>
                                        </p>
-                                       <a href="view_post.php?post_id=<?= $post_id; ?>"
+                                       <a href="blog-single-sidebar-left.php?post_id=<?= $post_id; ?>"
                                           class="read-more-btn icon-space-left">Leia mais
                                           <span class="icon">
                                              <i class="ion-ios-arrow-thin-right"></i>
