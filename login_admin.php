2c3679e6c0044424df83de4addbb7f38578e6119
Giovani <giovanilopesmcp@gmail.com>
2024-01-25 22:46:33 -0300

    fix tag html

--- login_admin.php
+++ login_admin.php
@@ -36,7 +36,7 @@ if (isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    <title>Admin/title>
+    <title>Admin</title>
 
     <!-- ::::::::::::::Favicon icon::::::::::::::-->
     <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/png">
